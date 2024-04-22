9d15ac27ce89add6eefd9d6a1acdc78dd1b7b244
sifat <sifat0666@gmail.com>
2024-04-22 22:07:43 +0600

    galaxy v1

--- src/script.js
+++ src/script.js
@@ -1,24 +1,62 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "lil-gui";
+// import { Points } from "three";
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI();
+
+// Canvas
 const canvas = document.querySelector("canvas.webgl");
 
-const sizes = { width: window.innerWidth, height: window.innerHeight };
+// Scene
 const scene = new THREE.Scene();
 
-const mesh = new THREE.Mesh(
-  new THREE.BoxGeometry(2, 3, 1),
-  new THREE.MeshBasicMaterial()
-);
-mesh.rotation.x = 2;
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load("/textures/particles/4.png");
 
-scene.add(mesh);
+const particlesGeaometry = new THREE.BufferGeometry();
+const count = 50000;
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
+const position = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
+
+for (let i = 0; i < count * 3; i++) {
+  position[i] = 50 * (Math.random() - 0.5);
+  colors[i] = Math.random();
+}
+
+particlesGeaometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(position, 3)
 );
-camera.position.z = 3;
+particlesGeaometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
+
+const particlesMaterial = new THREE.PointsMaterial();
+particlesMaterial.size = 0.5;
+particlesMaterial.alphaMap = texture;
+particlesMaterial.transparent = true;
+particlesMaterial.depthWrite = false;
+particlesMaterial.blending = THREE.AdditiveBlending;
+
+particlesMaterial.vertexColors = true;
+
+const particles = new THREE.Points(particlesGeaometry, particlesMaterial);
+scene.add(particles);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
 window.addEventListener("resize", () => {
   // Update sizes
@@ -34,8 +72,48 @@ window.addEventListener("resize", () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 10;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-renderer.render(scene, camera);
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
